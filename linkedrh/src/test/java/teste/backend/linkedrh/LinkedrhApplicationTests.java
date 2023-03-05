38ea64e9f847f9259f91ddc954096462e83f2f0b
edevaldojr <ed.tadsifpr@gmail.com>
2023-03-05 15:14:39 -0300

    Inicializando projeto

---
+++ linkedrh/src/test/java/teste/backend/linkedrh/LinkedrhApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.backend.linkedrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
